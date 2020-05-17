c6653626f8f6147053c3e2fe87e25a21868ce0d0
Kadir Şenkaya <senkayakadir@gmail.com>
2020-05-17 13:13:24 +0200

    Create Selecting_Random_Rows.sql

---
+++ 01-SQL_Recipes/Selecting_Random_Rows.sql
@@ -0,0 +1,25 @@
+-- Select randows rows
+-- Wiki: https://wiki.postgresql.org/wiki/TABLESAMPLE_Implementation
+
+-- Create an 'items' table with a million random numbers and strings
+CREATE TABLE items AS
+
+  SELECT
+    (random()*1000000)::integer AS n,
+    md5(random()::text) AS s
+  FROM
+    GENERATE_SERIES(1,1000000);
+
+-- Select 5 rows randomly
+SELECT *
+FROM mytable OFFSET floor(random()*N) 
+LIMIT 5;
+
+-- For big tables use TABLESAMPLE clause to retrieve a subset of the rows
+SELECT * 
+FROM items TABLESAMPLE SYSTEM (10);    --Returns about 10% of rows using SYSTEM method
+
+SELECT *
+FROM items TABLESAMPLE BERNOULLI (10); --Using BERNOULLI sampling method
+
+
